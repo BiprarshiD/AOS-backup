a291dbfa118b135134b513f1a38a0888b6a40ac2
Ishraq-R-Rahman <shwarup101@gmail.com>
2024-04-29 21:51:02 -0700

    :art:Final

--- create_queues.sh
+++ create_queues.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
+mkdir /dev/mqueue
+mount -t mqueue none /dev/mqueue
 
-sudo -u moderator pip install posix_ipc
+sudo -u moderator pip3 install posix_ipc
 sudo -u moderator python create_queue.py
 
 for i in {0..3}
 do
-    sudo -u player$i pip install posix_ipc
+    sudo -u player$i pip3 install posix_ipc
     sudo -u player$i python create_queue.py
 done
 
